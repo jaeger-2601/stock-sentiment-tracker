a72ca5bbb220616bf678600c77bbd6438d210fd9
Harshaa Vardaan <harshaaloga@gmail.com>
2022-10-22 16:11:58 +0530

    Add logging to aggregators

--- api/data_ingestion/aggregators/reddit-aggregator.py
+++ api/data_ingestion/aggregators/reddit-aggregator.py
@@ -1,10 +1,10 @@
 import os
 import praw
-from time import sleep
 from celery import Celery
-from typing import Generator
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
+from logging import getLogger
+from logging.config import fileConfig
 
 from data_ingestion.stocks import stocks_subreddits
 
@@ -12,28 +12,30 @@ load_dotenv()
 
 class RedditAggregator:
 
-    def __init__(self, client_id, client_secret):
+    def __init__(self, client_id:str, client_secret:str):
 
         self.reddit = praw.Reddit(
             client_id=client_id,
             client_secret=client_secret,
-            user_agent='Reddit Aggregator'
-            )
+            user_agent='Reddit Aggregator',
+            ratelimit_seconds=300
+        )
         self.celery = Celery(
             'data_ingestion.sentiment_analysis.sentiment_analysis', 
             broker='pyamqp://guest@localhost//'
         )
 
-    
-    def build_subreddit_instance(self):
+        fileConfig('logging.conf', defaults={'logfilename' : 'logs/reddit.log'})
+        self.logger = getLogger('RedditAggregator')
 
-        subreddits_rule = '+'.join(stocks_subreddits)
-        return self.reddit.subreddit(subreddits_rule)
-    
-    def aggregate_data(self, timeout:timedelta) -> Generator:
+        self.logger.info('Reddit aggregator initialized')
 
-        end_time = datetime.now() + timeout
-        subreddit_instance = self.build_subreddit_instance()
+
+
+    def run(self):
+
+        end_time = datetime.now() + timedelta(minutes=5)
+        subreddit_instance = self.reddit.subreddit('+'.join(stocks_subreddits))
         comment_count = 0
 
         for comment in subreddit_instance.stream.comments():
@@ -46,20 +48,10 @@ class RedditAggregator:
 
             if datetime.now() > end_time:
 
-                yield comment_count
-                end_time = datetime.now() + timeout
+                self.logger.info(f'Processed {comment_count} comments in 5 minutes')
+                end_time = datetime.now() + timedelta(minutes=5)
                 comment_count = 0
-
-    
-    def run(self):
-
-        for comment_count in self.aggregate_data(timedelta(minutes=5)):
-
-            print(f'Processed {comment_count} reddit comments in 5 minutes')
-            #sleep for 1 minute
-            sleep(60)
-
-            
+       
 
 
 if __name__ == '__main__':
--- api/data_ingestion/aggregators/twitter-aggregator.py
+++ api/data_ingestion/aggregators/twitter-aggregator.py
@@ -4,6 +4,8 @@ from celery import Celery
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from tweepy import StreamingClient, StreamRule, Tweet
+from logging import getLogger
+from logging.config import fileConfig
 
 from data_ingestion.stocks import djia_stocks
 
@@ -24,6 +26,10 @@ class TweetAggregator(StreamingClient):
             'data_ingestion.sentiment_analysis.sentiment_analysis', 
             broker='pyamqp://guest@localhost//'
         )
+        fileConfig('logging.conf', defaults={'logfilename' : 'logs/twitter.log'})
+        self.logger = getLogger('TwitterAggregator')
+
+        self.logger.info('Twitter aggregator initialized')
 
         super().__init__(*args, **kwargs)
 
@@ -47,50 +53,86 @@ class TweetAggregator(StreamingClient):
 
         # No rules exist
         if existing_rules.data is None:  # type: ignore
-            print('No rules found. Adding new rules')
+            self.logger.info('No rules found. Adding new rules')
             response = self.add_rules(self.rules)
         
         # If rules have changed
-        elif [ rule.value for rule in existing_rules.data] != self.rules:  # type: ignore
+        elif set(rule.value for rule in existing_rules.data) != set(rule.value for rule in self.rules):  # type: ignore
 
-            print('Rules have changed')
+            self.logger.info('Rules have changed. Updating existing rules to new rules')
             
             # Delete old rules
             self.delete_rules([ x.id for x in self.get_rules().data])  # type: ignore
             # Add new rules
             response = self.add_rules(self.rules)
         
-        if response.data is None :  # type: ignore
+        # Rules are same
+        else:
+            self.logger.info('Same stream rules found')
+        
+        if not response is None and response.data is None :  # type: ignore
+            self.logger.critical('Unable to update rules')
+            self.logger.critical(f'Twitter rule error: {response.errors[0]["details"]}') # type: ignore
             raise TwitterRuleError(response.errors[0]['details'])  # type: ignore
     
     def on_tweet(self, tweet: Tweet):
 
-
+        # Send tweet text to analyze and store in DB
         self.celery.send_task(
             'analyze_and_store',
             args=(tweet.text,),
         )
         self.tweet_count += 1
         
-
+        # Log processed tweet counts every 5 minutes
         if datetime.now() > self.end_time:
-            self.disconnect()
-    
-    def run(self):
 
-        self.add_default_rules()
-
-        while True:
+            self.logger.info(f'Processed {self.tweet_count} tweets in 5 minutes')
 
             self.end_time = datetime.now() + self.timeout
             self.tweet_count = 0
+    
+    def on_connection_error(self):
 
+        self.logger.error('Cannot connect to stream')
+
+        return super().on_connection_error()
+
+
+    def on_errors(self, errors):
+
+        self.logger.error('Errors recieved.')
+
+        for k, v in errors.items():
+            self.logger.error(f'{k}: {v}')
+
+        return super().on_errors(errors)
+    
+    def on_exception(self, exception):
+
+        self.logger.error('An unhandled exception has occured')
+        self.logger.exception(exception)
+
+        return super().on_exception(exception)
+    
+    def run(self):
+
+        backoff = 1
+
+        self.logger.info('Adding default rules')
+        self.add_default_rules()
+
+        self.end_time = datetime.now() + self.timeout
+
+        while True:
+            self.logger.info('Starting filter()')
             self.filter()
+            self.logger.info(f'Stream disconnected. Backing off for {2 ** backoff} seconds')
 
-            print(f'Processed {self.tweet_count} tweets in 5 minutes')
-            sleep(60)
+            sleep(2 ** backoff)
+            backoff += 1
 
 
 if __name__ == '__main__':
-    tweet_aggregator = TweetAggregator(os.environ['BEARER_TOKEN'])
+    tweet_aggregator = TweetAggregator(os.environ['BEARER_TOKEN'], wait_on_rate_limit=True)
     tweet_aggregator.run() 
